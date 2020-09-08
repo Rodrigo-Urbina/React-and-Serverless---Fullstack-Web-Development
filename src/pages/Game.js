1ce19d5b08cca817e571e776b65a76e847dcf1ff
Rodrigo-Urbina <rodrigourbinadelac@yahoo.com.mx>
2020-09-08 14:49:18 -0500

    registering score by user input

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
   StyledGame,
   StyledScore,
@@ -8,17 +8,26 @@ import {
 import { Strong } from "../styled/Random";
 
 export default function Game({ history }) {
-  const [score, setScore] = useState(1);
   const MAX_SECONDS = 90;
-  const [ms, setMs] = useState(0);
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  const [currentCharacter, setCurrentCharacter] = useState("a");
+  const [score, setScore] = useState(0);
+  const [ms, setMs] = useState(999);
   const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
+    setRandomCharacter();
     const currentTime = new Date();
     const interval = setInterval(() => updateTime(currentTime), 1);
     return () => clearInterval(interval);
     // eslint-disable-next-line no-use-before-define
   }, []);
+
+  const setRandomCharacter = () => {
+    const randomInt = Math.floor(Math.random() * 36);
+    setCurrentCharacter(characters[randomInt]);
+  };
 
   const updateTime = (startTime) => {
     const endDate = new Date();
@@ -34,37 +43,49 @@ export default function Game({ history }) {
     setMs(addLeadingZeros(updatedMs, 3));
   };
 
-  const addLeadingZeros = (num, length) => {
-    let zeros = "";
-    for (let i = 0; i < length; i++) {
-      zeros += "0";
-    }
-    return (zeros + num).slice(-length);
-  };
-
   useEffect(() => {
     if (seconds <= -1) {
+      //Todo: save the score
       history.push("/gameOver");
     }
   }, [seconds, ms, history]);
 
-  const keyUpHandler = (e) => {
-    console.log(e.key);
-  };
+  const keyUpHandler = useCallback(
+    (e) => {
+      console.log(e.key, currentCharacter);
+      if (e.key === currentCharacter) {
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        if (score > 0) {
+          setScore((prevScore) => prevScore - 1);
+        }
+      }
+      setRandomCharacter();
+    },
+    [currentCharacter]
+  );
 
   useEffect(() => {
     document.addEventListener("keyup", keyUpHandler);
     return () => {
       document.removeEventListener("keyup", keyUpHandler);
     };
-  }, []);
+  }, [keyUpHandler]);
+
+  const addLeadingZeros = (str, length) => {
+    let zeros = "";
+    for (let i = 0; i < length; i++) {
+      zeros += "0";
+    }
+    return (zeros + str).slice(-length);
+  };
 
   return (
     <StyledGame>
       <StyledScore>
         Score: <Strong>{score}</Strong>
       </StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>{currentCharacter}</StyledCharacter>
       <StyledTimer>
         Time:{" "}
         <Strong>
